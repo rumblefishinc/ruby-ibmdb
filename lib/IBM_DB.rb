f127fa697455e40b189f60976ee46fc36b5a4708
Ian MacDonald <imacdonald@rumblefish.com>
2016-10-26 14:38:07 -0700

    master: Refactor extension build

--- lib/IBM_DB.rb
+++ lib/IBM_DB.rb
@@ -1,24 +1,25 @@
-if(RUBY_PLATFORM =~ /darwin/i)
-   cliPackagePath = File.dirname(__FILE__)  + '/clidriver'
-        if(Dir.exists?(cliPackagePath))
-                currentPath = "#{File.expand_path(File.dirname(File.dirname(__FILE__)))}"
+# frozen_string_literal: true
+if RUBY_PLATFORM =~ /darwin/i
+  cli_package_path = File.dirname(__FILE__) + '/clidriver'
+  if Dir.exist?(cli_package_path)
+    current_path = File.expand_path(File.dirname(File.dirname(__FILE__))).to_s
 
-                cmd = "chmod 755 #{currentPath}/lib/ibm_db.bundle "
-                `#{cmd}`
+    cmd = "chmod 755 #{current_path}/lib/ibm_db.bundle "
+    `#{cmd}`
 
-                cmd = "chmod 755 #{currentPath}/lib/clidriver/lib/libdb2.dylib"
-                `#{cmd}`
+    cmd = "chmod 755 #{current_path}/lib/clidriver/lib/libdb2.dylib"
+    `#{cmd}`
 
-                cmd = "install_name_tool -change libdb2.dylib #{currentPath}/lib/clidriver/lib/libdb2.dylib #{currentPath}/lib/ibm_db.bundle"
-                `#{cmd}`
+    cmd = "install_name_tool -change libdb2.dylib #{current_path}/lib/clidriver/lib/libdb2.dylib #{current_path}/lib/ibm_db.bundle"
+    `#{cmd}`
 
-                $LOAD_PATH.unshift("#{currentPath}/lib")
-        end
+    $LOAD_PATH.unshift("#{current_path}/lib")
+  end
 
-        require 'ibm_db.bundle'
+  require 'ibm_db.bundle'
 
-elsif(RUBY_PLATFORM =~ /mswin32/ || RUBY_PLATFORM =~ /mingw32/ )
-        require 'mswin32/ibm_db'
+elsif RUBY_PLATFORM =~ /mswin32/ || RUBY_PLATFORM =~ /mingw32/
+  require 'mswin32/ibm_db'
 else
-        require 'ibm_db.so'
+  require 'ibm_db.so'
 end
